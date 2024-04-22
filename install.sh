42d790081e96acc9121516df67731a8c837930d8
grafst <grafst@users.noreply.github.com>
2024-04-22 16:28:47 +0200

    fix missing double quotes

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 chmod +x get-phonebook.sh
-echo */30 * * * * /home/pi/kurt/get-phonebook.sh | sudo tee /etc/crontab
+echo "*/30 * * * * /home/pi/kurt/get-phonebook.sh" | sudo tee -a /etc/crontab
 sudo cp -f kurt.service /lib/systemd/system/kurt.service
 sudo chmod 644 /lib/systemd/system/kurt.service
 sudo systemctl daemon-reload
